12d212f0fcc832bdf23fabec5615ea3c917489be
zzangse1 <zzangse1014@gmail.com>
2025-09-08 21:13:28 +0900

    앱 모듈 정의

---
+++ app/src/main/java/com/likelion/zzangsepokemon/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.likelion.zzangsepokemon.di
+
+import com.likelion.zzangsepokemon.data.remote.BASE_URL
+import com.likelion.zzangsepokemon.data.remote.PokeApi
+import com.likelion.zzangsepokemon.repository.PokemonRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun providePokemonRepository(
+        api: PokeApi
+    ) = PokemonRepository(api)
+
+    @Singleton
+    @Provides
+    fun providePokeApi(): PokeApi {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PokeApi::class.java)
+    }
+}
