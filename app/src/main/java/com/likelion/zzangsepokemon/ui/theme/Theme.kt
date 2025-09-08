3e35dd35b02d3cf27068a8395e0492dcefd136f3
zzangse1 <zzangse1014@gmail.com>
2025-09-08 21:14:20 +0900

    앱 테마 설정

---
+++ app/src/main/java/com/likelion/zzangsepokemon/ui/theme/Theme.kt
@@ -0,0 +1,55 @@
+package com.likelion.zzangsepokemon.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Color(0xFFE1BEE7),
+    background = Color(0xFF101010),
+    onBackground = Color.White,
+    surface = Color(0xFF303030),
+    onSurface = Color.White
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Color.Blue,
+    background = LightBlue,
+    onBackground = Color.Black,
+    surface = Color.White,
+    onSurface = Color.Black
+)
+
+@Composable
+fun ZzangSePokemonTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        shapes = Shapes,
+        typography = Typography,
+        content = content
+    )
+}
